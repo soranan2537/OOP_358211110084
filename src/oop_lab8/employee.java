c84bad65a1de15b7c5f3aaf1c4baa6d2c40c7348
com ok <soranan2537>
2018-02-13 12:04:22 +0700

    employee

---
+++ src/oop_lab8/employee.java
@@ -0,0 +1,47 @@
+package oop_lab8;
+
+public class employee {
+    private String employeeID;
+    private String position;
+    private String salary;
+
+    public Employee(String employeeID, String position, String salary) {
+        this.employeeID = employeeID;
+        this.position = position;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeID='" + employeeID + '\'' +
+                ", position='" + position + '\'' +
+                ", salary='" + salary + '\'' +
+                '}';
+    }
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+}
+}
