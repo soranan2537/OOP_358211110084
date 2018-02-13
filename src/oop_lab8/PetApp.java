84d176065bfcdbbb0dd618db58ba12dc17946376
com ok <soranan2537>
2018-02-13 12:18:22 +0700

    pet

---
+++ src/oop_lab8/PetApp.java
@@ -0,0 +1,22 @@
+package oop_lab8;
+
+public class PetApp {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Chayen",2,"Bangkeaw");
+        System.out.println(dog.toString());
+        dog.makeNoise();
+        dog.myPet();
+
+        Cat cat = new Cat("Giant",2,"persian");
+        System.out.println(cat.toString());
+        cat.makeNoise();
+        cat.myPet();
+
+        Cow cow = new Cow("Namjai", 5);
+        System.out.println(cow.toString());
+        cow.makeNoise();
+        cow.myPet();
+
+    }//main
+}//class
+}
