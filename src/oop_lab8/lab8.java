6ad96731667fc9a572f6e8e633df31402bfc4ca8
com ok <soranan2537>
2018-02-13 11:11:16 +0700

    pet

--- src/oop_lab8/lab8.java
+++ src/oop_lab8/lab8.java
@@ -1,5 +1,32 @@
 package oop_lab8;
 
 public class lab8 {
+    public class Cat extends Pet {
+        private String breed;
 
-}
+        public Cat(String name, int age, String breed) {
+            super(name, age);
+            this.breed = breed;
+        }
+
+        @Override
+        public String toString() {
+            return "Cat{" +
+                    "breed='" + breed + '\'' +
+                    "} " + super.toString();
+        }
+
+        public String getBreed() {
+            return breed;
+        }
+
+        public void setBreed(String breed) {
+            this.breed = breed;
+        }
+
+
+        @Override
+        public void makeNoise() {
+            System.out.println("Meaw-Meaw !!!");
+        }
+    }
