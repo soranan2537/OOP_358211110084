d87e8de0a2f136419cf152ca4f16b14d6c8ab7c1
com ok <soranan2537>
2018-02-13 12:15:43 +0700

    pet

---
+++ src/oop_lab8/pet.java
@@ -0,0 +1,42 @@
+package oop_lab8;
+
+public class pet {
+    String s;
+    private String name;
+    private int age;
+
+    public Pet(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    protected void myPet(){
+        System.out.println(this.name);
+    }
+    //Abstract Method
+    public abstract void makeNoise();
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+}
