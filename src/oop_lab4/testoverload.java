8d69ee6c9b9c0e121886465893b8cd105de65acd
com ok <soranan2537>
2018-02-27 02:00:14 +0700

    overloadmethod

---
+++ src/oop_lab4/testoverload.java
@@ -0,0 +1,38 @@
+package oop_lab4;
+
+public class testoverload {
+    public class TestMethods {
+        //global variabla
+        public static int number =1000;
+        //Method
+        //type 1
+        public static void A(){
+            System.out.println("Hello A");
+            B(20);
+
+        }//A
+        //type 2
+        public static void B (int x){
+            System.out.println("Hello B "+x);
+
+        }//B
+        //type 3
+        public static int C (int x,int y){
+            System.out.println("Hello C"+number);
+            return x+y;
+        }//C
+
+        public static void main(String[] args){
+            System.out.println("Hello Main");
+            // clling method
+            //  A();
+            //  B(10);
+            int number = C(10,20);
+            System.out.println(number);
+            System.out.println(C(50,50));
+            //local variable
+            int x = 100;
+
+
+        }//main
+    }//class
