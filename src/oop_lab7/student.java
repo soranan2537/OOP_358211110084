6b18d2b5b8217be4c5db7d18b9548d67596e26c0
com ok <soranan2537>
2018-04-03 13:45:04 +0700

    student

--- src/oop_lab7/student.java
+++ src/oop_lab7/student.java
@@ -4,64 +4,63 @@ package oop_lab7;
 // 2.major
 // 3.Faculty
 // 4.Name
+public class student {
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", major='" + major + '\'' +
+                ", faculty='" + faculty + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 
-    public class Student {
-        @Override
-        public String toString() {
-            return "Student{" +
-                    "id='" + id + '\'' +
-                    ", major='" + major + '\'' +
-                    ", faculty='" + faculty + '\'' +
-                    ", name='" + name + '\'' +
-                    '}';
-        }
+    private String id ;
+    private String major ;
+    private String faculty ;
+    private String name ;
+    //constructor
+    //Default constructor
+    public Student (){}
+    //create constructor by owner
+    public Student (String id,String m,String f,String n){
+        //asigne data to class properties
+        this.id =id;
+        this.faculty = f;
+        this.major = m;
+        this.name = n;
+    }
+    //getter and setter methods
 
-        private String id ;
-        private String major ;
-        private String faculty ;
-        private String name ;
-        //constructor
-        //Default constructor
-        public Student (){}
-        //create constructor by owner
-        public Student (String id,String m,String f,String n){
-            //asigne data to class properties
-            this.id =id;
-            this.faculty = f;
-            this.major = m;
-            this.name = n;
-        }
-        //getter and setter methods
+    public String getId() {
+        return id;
+    }
 
-        public String getId() {
-            return id;
-        }
+    public void setId(String id) {
+        this.id = id;
+    }
 
-        public void setId(String id) {
-            this.id = id;
-        }
+    public String getMajor() {
+        return major;
+    }
 
-        public String getMajor() {
-            return major;
-        }
+    public void setMajor(String major) {
+        this.major = major;
+    }
 
-        public void setMajor(String major) {
-            this.major = major;
-        }
+    public String getFaculty() {
+        return faculty;
+    }
 
-        public String getFaculty() {
-            return faculty;
-        }
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
 
-        public void setFaculty(String faculty) {
-            this.faculty = faculty;
-        }
+    public String getName() {
+        return name;
+    }
 
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-    }//calss
+    public void setName(String name) {
+        this.name = name;
+    }
+}//calss
