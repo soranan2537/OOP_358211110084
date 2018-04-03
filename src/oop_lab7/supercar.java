6d6f88b96d0f5fa1b205466f17adb18000e52f79
com ok <soranan2537>
2018-04-03 13:47:45 +0700

    superCAR

---
+++ src/oop_lab7/supercar.java
@@ -0,0 +1,51 @@
+package oop_lab7;
+//1.car brand
+//2.car color
+//3.car engine size
+//4.max speed
+//5. country of origin
+public class supercar {
+    private String carBrand;
+    private String carColor;
+    private String engSize;
+
+    public SuperCer (){}
+    public SuperCer(String b,String c,String e){
+        this.carBrand = b;
+        this.carColor = c;
+        this.engSize = e;
+    }
+
+    public String getSuperCarInfo() {
+        return "SuperCar{" +
+                "carBrand='" + carBrand + '\'' +
+                ", carColor='" + carColor + '\'' +
+                ", engSize='" + engSize + '\'' +
+                '}';
+    }
+
+
+    public String getCarBrand() {
+        return carBrand;
+    }
+
+    public void setCarBrand(String carBrand) {
+        this.carBrand = carBrand;
+    }
+
+    public String getEngSize() {
+        return engSize;
+    }
+
+    public void setEngSize(String engSize) {
+        this.engSize = engSize;
+    }
+
+    public String getCarColor() {
+        return carColor;
+    }
+
+    public void setCarColor(String carColor) {
+        this.carColor = carColor;
+    }
+}
